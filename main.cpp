b43c628df9f1c19ad2d01c3b031f26e53b6ee0d0
Dev-LinhPham <87310137+Dev-LinhPham@users.noreply.github.com>
2023-08-26 11:09:18 +0700

    Add class Git

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 
 using namespace std;
 
+class Git
+{
+public:
+    string directory;
+    void push();
+    void commit();    
+};
+
+void Git::commit()
+{
+
+}
+
+void Git::push()
+{
+    
+}
+
+
 int main(int argc, char const *argv[])
 {
     cout << "Hello World" << endl;
